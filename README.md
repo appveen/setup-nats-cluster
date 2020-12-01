9257d4bb125e77551d12409659117fe78a1966c8
Jerry M <developer.jerrymannel@gmail.com>
2020-12-01 08:46:20 +0530

    updated and simplified setup steps

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# NATS Cluster POC
+# NATS Streaming cluster setup
 
-## Steps to install nats streaming cluster
- - Run the following command
-   ```
-    sudo ./install.sh
-   ``` 
- - Specify the k8s namespace and cluster size as prompted
+This is a tailored version of NATS Streaming cluster setup for data.stack.
 
-## Steps to uninstall nats streaming cluster
- - Run the following command
-   ```
-    sudo ./uninstall.sh
-   ```
- - Specify the k8s namespace from where you want to uninstall
+The original document can be found under the NATS documentation - [Basic NATS and NATS Streaming Setup](https://docs.nats.io/nats-on-kubernetes/minimal-setup#ha-setup-using-statefulsets)
 
-## References
- - https://github.com/nats-io/nats-streaming-operator
+# Install
+
+To install the cluster run `./install.sh`. Once the services are up connect to nats streaming server using the url - `nats://nats.<namespace>:4222`
+
+# Uninstall
+
+To teardown the cluster run `./uninstall.sh` 
+
+# Changes from the original documentation
+
+* Cluster ID is set to `odp-cluster`
+* Persistent volume configuration has been removed.
--- uninstall.sh
+++ uninstall.sh
@@ -1,25 +1,9 @@
 #!/bin/sh
-echo "********Uninstalling NATS Cluster On K8S********"
-read -p "Enter Kubernetes Namespace : " namespace
-cp 00-prereqs.yaml 00-prereqs-$namespace.yaml
-cp 10-deployment.yaml 10-deployment-$namespace.yaml 
-cp default-rbac.yaml default-rbac-$namespace.yaml
-cp deployment.yaml deployment-$namespace.yaml
-cp nats.yaml nats-$namespace.yaml
-sed -i "s/__NAMESPACE__/$namespace/" 00-prereqs-$namespace.yaml
-sed -i "s/__NAMESPACE__/$namespace/" 10-deployment-$namespace.yaml
-sed -i "s/__NAMESPACE__/$namespace/" default-rbac-$namespace.yaml
-sed -i "s/__NAMESPACE__/$namespace/" deployment-$namespace.yaml
-sed -i "s/__NAMESPACE__/$namespace/" nats-$namespace.yaml
-kubectl delete -f nats-$namespace.yaml
-kubectl delete -f deployment-$namespace.yaml
-kubectl delete -f default-rbac-$namespace.yaml
-kubectl delete -f 10-deployment-$namespace.yaml
-kubectl delete -f 00-prereqs-$namespace.yaml
-kubectl delete natscluster --all -n $namespace
-rm 00-prereqs-$namespace.yaml
-rm 10-deployment-$namespace.yaml 
-rm default-rbac-$namespace.yaml
-rm deployment-$namespace.yaml
-rm nats-$namespace.yaml
-echo "********NATS Cluster Uninstalled On K8S*********"
+echo "Tearing down the NATS Streaming cluster"
+read -p "Enter the namespace : " namespace
+cp cluster-nats.yaml cluster-nats-$namespace.yaml
+cp cluster-stan.yaml cluster-stan-$namespace.yaml
+sed -i "s/__namespace__/$namespace/" cluster-nats-$namespace.yaml
+sed -i "s/__namespace__/$namespace/" cluster-stan-$namespace.yaml
+kubectl delete -f cluster-nats-$namespace.yaml
+kubectl delete -f cluster-stan-$namespace.yaml
